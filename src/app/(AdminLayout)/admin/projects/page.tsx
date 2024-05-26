69b1933a43937fbb6643e072f96389523d40a3de
asheque33 <ashequrrahman3333@gmail.com>
2024-05-26 23:55:51 +0600

    creating a project page form

--- src/app/(AdminLayout)/admin/projects/page.tsx
+++ src/app/(AdminLayout)/admin/projects/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const ProjectsPage = () => {
-  return <div>Create a project Page</div>;
+  const handleCreateProject = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleCreateProject}>
+      <input type="text" name="title" id="title" />
+      <input type="text" name="description" id="description" />
+      <input type="text" name="image" id="image" />
+      <input type="text" name="live-link" id="live-link" />
+      <input type="text" name="client-link" id="client-link" />
+      <input type="text" name="server-link" id="server-link" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default ProjectsPage;
